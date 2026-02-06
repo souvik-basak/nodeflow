db095cfb4cc4bb625cba3fa0bbd2047a0d4f0be9
Souvik <souvik.basak2404@gmail.com>
2026-02-06 15:21:06 +0530

    feat: integrate Prisma with PostgreSQL and set up initial database schema
    - Added Prisma dependencies to package.json
    - Created prisma.config.ts for Prisma configuration
    - Added initial migration SQL for User and Post tables
    - Created migration lock file for version control
    - Defined Prisma schema with User and Post models
    - Updated page component to fetch and display users from the database
    - Implemented database connection logic in db.ts using Prisma and PostgreSQL adapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
-const page = () => {
+import prisma from "@/lib/db";
+const page = async() => {
+
+  const users = await prisma.user.findMany();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <div>{JSON.stringify(users)}</div>
       <Button className="bg-amber-400">Click me!</Button>
     </div>
   );
