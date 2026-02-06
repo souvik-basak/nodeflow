c42851adcaac0148aaaca1628caf35c5b023997e
Souvik <souvik.basak2404@gmail.com>
2026-02-06 23:21:44 +0530

    feat: set up tRPC with React Query and implement user fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-import { Button } from "@/components/ui/button";
-import prisma from "@/lib/db";
-const page = async() => {
+import { getQueryClient, trpc } from "../trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
-  const users = await prisma.user.findMany();
+const page = async() => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <div>{JSON.stringify(users)}</div>
-      <Button className="bg-amber-400">Click me!</Button>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 };
