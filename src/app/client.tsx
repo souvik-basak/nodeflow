6e869e36dcd9521637d341c640ab3901ecfd99d4
Souvik <souvik.basak2404@gmail.com>
2026-02-06 23:30:02 +0530

    refactor: remove unused import of trpc from server in Client component

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useTRPC } from "@/trpc/client"
-import { trpc } from "@/trpc/server"
 import { useSuspenseQuery } from "@tanstack/react-query"
 
 export const Client = () =>{
